1b6d0ebef0c4a268ce3cba2c1246218ebc0ad88e
Genetic <shae.briggs2@gmail.com>
2017-06-08 17:23:19 +1000

    Update Functions.hpp

--- Roamingdm.Takistan/Functions.hpp
+++ Roamingdm.Takistan/Functions.hpp
@@ -1,3 +1,16 @@
+class Socket_Reciever {
+    tag = "SOCK";
+    class SQL_Socket {
+        file = "core\DB";
+        class dataQuery {};
+        class insertPlayerInfo {};
+        class requestReceived {};
+        class syncData {};
+        class updatePartial {};
+        class updateRequest {};
+    };
+};
+
 class RoamingDM_core {
 	tag = "rDM";
 
@@ -7,11 +20,43 @@ class RoamingDM_core {
 	class Itemspawns {};
 	};
 
+	class initClient {
+		file = "Core\initClient";
+		class Vars {};
+		class Action {};
+	};
+
+	class Admin {
+		file = "Core\Admin";
+		class Admin_Brick {};
+		class Admin_Freeze {};
+		class AdminArsenal {};
+		class AdminBrick {};
+		class AdminCall {};
+		class AdminFreeze {};
+		class AdminGinfo {};
+		class AdminGodMode {};
+		class AdminMarks {};
+		class AdminNoMarks {};
+		class adminTeleport {};
+		class Teleport {};
+		class vGod {};
+
+	};
+
 	class Core {
 		file = "Core\clientFunctions";
-		class keyHandler {};
-		class playerGear {};
-		class vehicleRepair {};
 		class Checks {};
+		class escSave {};
+		class fetchCfgDetails {};
+		class handleItem {};
+		class keyHandler {};
+		class loadGear {};
+		class playerGear {};
+		class Sanity {};
+		class saveGear {};
+		class SetupHud {};
+		class UpdateHud {};
+		class vehicleRepair {};
 	};
 };
