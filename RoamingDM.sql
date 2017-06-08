07ae641bec1970b3c5078e64d42646a3faa5e87b
Genetic <shae.briggs2@gmail.com>
2017-06-08 18:31:37 +1000

    Create RoamingDM.sql

--- RoamingDM.sql
+++ RoamingDM.sql
@@ -3,8 +3,8 @@ SET time_zone = "+00:00";
 
 -- Default Schema
 --
-CREATE DATABASE IF NOT EXISTS `RoamingDM` DEFAULT CHARACTER SET utf8mb4;
-USE `RoamingDM`;
+CREATE DATABASE IF NOT EXISTS `RoamingDM2` DEFAULT CHARACTER SET utf8mb4;
+USE `RoamingDM2`;
 
 
 
@@ -18,18 +18,18 @@ CREATE TABLE IF NOT EXISTS `players` (
   `uid` int(6) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `aliases` text NOT NULL,
-  `pid` varchar(17) NOT NULL,
-  `cash` int(100) NOT NULL DEFAULT '0',
-  `bankacc` int(100) NOT NULL DEFAULT '0',
+  `Steam64` varchar(17) NOT NULL,
+  `CASH` int(100) NOT NULL DEFAULT '0',
+  `BANK` int(100) NOT NULL DEFAULT '0',
   `Player_gear` text NOT NULL,
-  `adminlevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
-  `blacklist` tinyint(1) NOT NULL DEFAULT '0',
+  `adminlvl` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
+  `Blacklist` tinyint(1) NOT NULL DEFAULT '0',
   `insert_time` timestamp DEFAULT CURRENT_TIMESTAMP,
   `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`uid`),
-  UNIQUE KEY `pid` (`pid`),
+  UNIQUE KEY `Steam64` (`Steam64`),
   KEY `name` (`name`),
-  KEY `blacklist` (`blacklist`)
+  KEY `Blacklist` (`Blacklist`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
